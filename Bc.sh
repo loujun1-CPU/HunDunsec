144d8fdef47c6f023afbbed417c1f0c736d8abdc
loujun1-CPU <2834806242@qq.com>
2025-11-27 11:53:14 +0800

    Update Bc.sh

--- Bc.sh
+++ Bc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-DVWA_COMPOSE="/root/HunDun/dvwa.yaml"
-PIKACHU_COMPOSE="/root/HunDun/pikachu.yaml"
+DVWA_COMPOSE="/root/HunDunsec/dvwa.yaml"
+PIKACHU_COMPOSE="/root/HunDunsec/pikachu.yaml"
 get_ip_address() {
     if command -v python3 &> /dev/null; then
         python3 - << EOF
